94d2214638c6737551c56f48f683a9e16a5e6fe9
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-04-26 10:11:25 +0300

    install next using npx create-next-app

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import style from '../styles/Home.module.css';
+
+const Home = () => (
+	<div>
+		<h1 className={style.title}>Todo</h1>
+	</div>
+);
+export default Home;
